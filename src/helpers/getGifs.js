085c950482aa0d68009fc8ca9d17cb3aa9c90a63
fhios - Aurelien Jove <aurelien.jove@fhios.es>
2024-09-09 10:03:36 +0200

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,13 @@
+export const getGifs = async(category) => {
+    const apiKey = '8A0Egp6GPXozJdJWFXCJifA206HxK8z8';
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=5`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+    const gifs = data.map(img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+
+    return gifs;
+}
